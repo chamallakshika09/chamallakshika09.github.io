450dd5beadd251bea90d44d12454510b5e2fa853
Chamal Perera <chamallakshika09@gmail.com>
2023-11-16 12:04:46 +0530

    add carousel for project preview

--- src/components/projects-preview.jsx
+++ src/components/projects-preview.jsx
@@ -1,27 +1,91 @@
 import React from 'react';
-import { Typography, Card, CardActionArea, CardMedia, CardContent, Grid, Button, Stack } from '@mui/material';
+import { Typography, Card, CardActionArea, CardMedia, CardContent, Button, Stack } from '@mui/material';
+import Slider from 'react-slick';
 import { projectsData } from 'data';
 import { useNavigate } from 'react-router-dom';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import IconButton from '@mui/material/IconButton';
+
+const PrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <IconButton
+      onClick={onClick}
+      sx={{ left: '-40px', display: 'block', top: '50%', transform: 'translate(0, -50%)', position: 'absolute' }}
+    >
+      <NavigateBeforeIcon />
+    </IconButton>
+  );
+};
+
+const NextArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <IconButton
+      onClick={onClick}
+      sx={{ right: '-40px', display: 'block', top: '50%', transform: 'translate(0, -50%)', position: 'absolute' }}
+    >
+      <NavigateNextIcon />
+    </IconButton>
+  );
+};
 
 export const ProjectsPreview = () => {
   const navigate = useNavigate();
+
+  const settings = {
+    // dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    cssEase: 'linear',
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          // dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <Stack alignItems="center" spacing={1} sx={{ mt: 4, p: 4 }}>
       <Typography variant="h4" gutterBottom textAlign="center">
         Featured Projects
       </Typography>
-      <Grid container spacing={4} justifyContent="center">
-        {projectsData
-          .filter((project) => project.featuredProject)
-          .map((project, index) => (
-            <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
-              <Card>
+
+      <Stack sx={{ width: '80%', flexGrow: 1 }}>
+        <Slider {...settings}>
+          {projectsData
+            // .filter((project) => project.featuredProject)
+            .map((project) => (
+              <Card key={project.id}>
                 <CardActionArea href={project.projectUrl}>
                   <CardMedia
                     component="img"
                     image={project.imageUrl}
                     alt={project.title}
-                    sx={{ height: 100, width: 'auto' }}
+                    sx={{ height: 140, width: 'auto' }}
                   />
                   <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
@@ -33,9 +97,9 @@ export const ProjectsPreview = () => {
                   </CardContent>
                 </CardActionArea>
               </Card>
-            </Grid>
-          ))}
-      </Grid>
+            ))}
+        </Slider>
+      </Stack>
 
       <Button variant="contained" onClick={() => navigate('/projects')} sx={{ mt: 2 }}>
         View Projects
